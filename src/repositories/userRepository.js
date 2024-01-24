a41fed8706c16aac507f9ff6a37df0bb79ccb253
Daniels <109157434+banksdaniel@users.noreply.github.com>
2024-01-24 19:37:55 -0300

    feat: define registration and login HTTP routes

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,9 @@
+
+const User = require('../entities/user'); 
+const db = require('../adapters/database'); s
+
+exports.saveUser = async (user) => {
+};
+
+exports.findUserByUsername = async (username) => {
+};
