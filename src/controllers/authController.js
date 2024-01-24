086365593275cb6c4fa6c505cb80ae2ab1fd8bbc
Daniels <109157434+banksdaniel@users.noreply.github.com>
2024-01-24 19:38:30 -0300

    feat: add registration and login controllers & configure database connection

---
+++ src/controllers/authController.js
@@ -0,0 +1,8 @@
+const User = require('../entities/user'); 
+const authService = require('../usecases/authUseCase'); 
+
+exports.registerUser = async (req, res) => {
+};
+
+exports.loginUser = async (req, res) => {
+};
