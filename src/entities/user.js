d62dd96325dc0a749bfd89ac6976af9abb2115fc
Daniels <109157434+banksdaniel@users.noreply.github.com>
2024-01-31 19:33:18 -0300

    feat): implement user registration and login logic

--- src/entities/user.js
+++ src/entities/user.js
@@ -1,8 +1,46 @@
-class User {
-    constructor(username, password) {
-      this.username = username;
-      this.password = password;
+const User = require('../entities/user');
+const authService = require('../controllers/authController.js');
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+   
+    const existingUser = await authService.findUserByUsername(username);
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already in use' });
     }
+
+   
+    const newUser = new User(username, password);
+
+   
+    const registeredUser = await authService.registerUser(newUser);
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error registering user' });
   }
-  
-  module.exports = User;
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+
+    const authenticatedUser = await authService.authenticateUser(username, password);
+
+    if (!authenticatedUser) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+ 
+    const token = authService.generateAuthToken(authenticatedUser);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error logging in' });
+  }
+};
