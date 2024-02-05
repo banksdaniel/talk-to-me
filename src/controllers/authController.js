61422365e165b910de703836ddb19c0ece9dbe92
Daniels <109157434+banksdaniel@users.noreply.github.com>
2024-02-05 18:08:55 -0300

    feat(auth): include authentication token in login response

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,22 @@
-const User = require('../entities/user'); 
-const authService = require('../usecases/authUseCase'); 
-
-exports.registerUser = async (req, res) => {
-};
+const jwt = require('jsonwebtoken');
+const User = require('../entities/user');
+const authService = require('../usecases/authUseCase');
 
 exports.loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const authenticatedUser = await authService.authenticateUser(username, password);
+
+    if (!authenticatedUser) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const token = authService.generateAuthToken(authenticatedUser);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error logging in' });
+  }
 };
