0f10c8fda27c6c2633765c53a5409aa51e797f48
Daniels <109157434+banksdaniel@users.noreply.github.com>
2024-02-09 11:37:31 -0300

    feat(repositories): implement user repository functions

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,9 +1,20 @@
-
-const User = require('../entities/user'); 
-const db = require('../adapters/database'); s
+const User = require('../entities/user');
+const db = require('../adapters/database');
 
 exports.saveUser = async (user) => {
+  try {
+    const savedUser = await db.save(user);
+    return savedUser;
+  } catch (error) {
+    throw new Error('Error saving user');
+  }
 };
 
 exports.findUserByUsername = async (username) => {
+  try {
+    const user = await db.findOne({ username });
+    return user;
+  } catch (error) {
+    throw new Error('Error finding user by username');
+  }
 };
