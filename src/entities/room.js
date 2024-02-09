29d12693d0b4821b21c6394c659e333c3c45875c
Daniels <109157434+banksdaniel@users.noreply.github.com>
2024-02-09 11:38:06 -0300

    style(entities): remove comments from Room class

--- src/entities/room.js
+++ src/entities/room.js
@@ -2,8 +2,8 @@ class Room {
     constructor(name, topic) {
       this.name = name;
       this.topic = topic;
-      this.users = []; // Pode conter a lista de usuários na sala
-      this.messages = []; // Pode conter o histórico de mensagens da sala
+      this.users = []; 
+      this.messages = []; 
     }
   }
   
