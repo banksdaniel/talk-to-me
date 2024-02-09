43df1da2ff05e1407e194b071f0d047c45b1d1c8
Daniels <109157434+banksdaniel@users.noreply.github.com>
2024-02-09 11:37:21 -0300

    feat(auth): include token in login response

--- src/routes/authController.js
+++ src/routes/authController.js
@@ -1,8 +1,44 @@
-const User = require('../entities/user'); 
-const authService = require('../usecases/authUseCase'); 
+const jwt = require('jsonwebtoken');
+const User = require('../entities/user');
+const authService = require('../usecases/authUseCase');
 
 exports.registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await authService.findUserByUsername(username);
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already in use' });
+    }
+
+    const newUser = new User(username, password);
+
+    const registeredUser = await authService.registerUser(newUser);
+
+    const token = authService.generateAuthToken(registeredUser);
+
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error registering user' });
+  }
 };
 
 exports.loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const authenticatedUser = await authService.authenticateUser(username, password);
+
+    if (!authenticatedUser) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const token = authService.generateAuthToken(authenticatedUser);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error logging in' });
+  }
 };
